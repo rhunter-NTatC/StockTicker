574669149731d5900ced7b448976ba77970521df
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-11-30 12:09:09 -0600

    init

---
+++ src/stockTicker/DataRetrieval.java
@@ -0,0 +1,17 @@
+package stockTicker;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class DataRetrieval {
+
+	
+	
+	
+	
+	
+	
+}
---
+++ src/stockTicker/StockTicker.java
@@ -0,0 +1,12 @@
+package stockTicker;
+
+/*
+ * Instructions found here:
+ * https://docs.google.com/document/d/1m9VsY44Jn6BiUf5aGWCtjUmvvtaUPX0TFx20aVikAjs/edit?usp=sharing
+ * 
+ */
+
+
+public class StockTicker {
+
+}
